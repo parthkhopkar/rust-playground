b9251b9dbc2f1343db5bf6cdac46ff813a13d639
Parth Khopkar <parth.khopkar96@gmail.com>
2020-05-11 12:16:36 -0500

    Said hello to Cargo

---
+++ hello-cargo/target/debug/deps/hello_cargo-de94115b39f8d462.d
@@ -0,0 +1,5 @@
+C:\Parth\CS Projects\rust-playground\hello-cargo\target\debug\deps\hello_cargo-de94115b39f8d462.rmeta: src\main.rs
+
+C:\Parth\CS Projects\rust-playground\hello-cargo\target\debug\deps\hello_cargo-de94115b39f8d462.d: src\main.rs
+
+src\main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+C:\Parth\CS\ Projects\rust-playground\hello-cargo\target\debug\hello-cargo.exe: C:\Parth\CS\ Projects\rust-playground\hello-cargo\src\main.rs
